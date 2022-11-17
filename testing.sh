c5b78123705412b9da5549ac321e77158d550423
“lpears201” <lpears201@caledonian.ac.uk>
2022-11-17 18:27:56 +0000

    fixing issues with test

--- testing.sh
+++ testing.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 javac CurrencyConverter.java
 
-input = "1 pounds"
-actual = "java CurrencyConverter $input"
-expected = "1 Pound = 1.36 Dollars 1 Pound = 1.19 Euros Thank you for using the converter."
+input="1 pounds"
+actual="java CurrencyConverter $input"
+expected="1 Pound = 1.36 Dollars 1 Pound = 1.19 Euros Thank you for using the converter."
 
 
 if [[ "$expected" == "$actual" ]]; then
