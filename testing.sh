e9bf19908e1ff6104a923d8adb2f06a504cbd9bd
“lpears201” <lpears201@caledonian.ac.uk>
2022-11-17 18:13:04 +0000

    fixing issues with test

--- testing.sh
+++ testing.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-intput = "1 pounds"
-acttual = $[java CurrencyConverter 1 pounds]
+javac CurrencyConverter.java
+
+input = "1 pounds"
+actual = $[java CurrencyConverter $input]
 expected = "1 Pound = 1.36 Dollars 1 Pound = 1.19 Euros Thank you for using the converter."
 
 
-if [[ "$expected" == "$acttual" ]]; then
+if [[ "$expected" == "$actual" ]]; then
     echo "Strings are equal."
 else
     echo "Strings are not equal."
 fi
-
